a5df38feca1d93a4d8f0a9f13d94bd87f41050df
Clint Jones <clinton.alan.jones@gmail.com>
2025-11-21 11:15:43 -0800

    Using ViewState for postback

--- crustysite/Counter.aspx.cs
+++ crustysite/Counter.aspx.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.Specialized;
 using System.Linq;
 using System.Web;
 using System.Web.UI;
@@ -11,15 +12,38 @@ namespace crustysite
     {
         protected void Page_Load(object sender, EventArgs e)
         {
-            CounterOP.Text = Convert.ToString(0);
+            // When you click add it will send back 0 every time
+            // Maintain a state (ViewState)
+            // CounterOP.Text = Convert.ToString(0);
+
+            /* 
+             * This runs when the page first loads, not 
+             * subsequent postbacks (like button clicks)
+             */
+
+            if (!IsPostBack) { 
+                ViewState["count"] = 0;
+                CounterOP.Text = "0";
+            }
+            
         }
 
         protected void Add_Click(object sender, EventArgs e)
         {
-            // this won't increase beyond 1 because the form uses postback
-            int value = Convert.ToInt32(CounterOP.Text);
+            /* 
+             * This doesn't increase beyond 1 because the form uses postback
+             * We need to capture the state between postbacks (using ViewState)
+             */
+            /* int value = Convert.ToInt32(CounterOP.Text);
 
             CounterOP.Text = Convert.ToString(++value);
+            */
+
+            // Implement the viewstate logic to explicitly store the count value instead
+            int count = (int)ViewState["count"];
+
+            ViewState["count"] = ++count;
+            CounterOP.Text = count.ToString();
         }
     }
 }
