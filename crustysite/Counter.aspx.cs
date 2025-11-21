5d619b95106bc5fcbaf23d660d519235c5cd7f2d
Clint Jones <clinton.alan.jones@gmail.com>
2025-11-21 10:40:39 -0800

    Buggy code without using ViewState for postbacks

---
+++ crustysite/Counter.aspx.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace crustysite
+{
+    public partial class Counter : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            CounterOP.Text = Convert.ToString(0);
+        }
+
+        protected void Add_Click(object sender, EventArgs e)
+        {
+            // this won't increase beyond 1 because the form uses postback
+            int value = Convert.ToInt32(CounterOP.Text);
+
+            CounterOP.Text = Convert.ToString(++value);
+        }
+    }
+}
